340bdbefab17a039c8daa1cd80cee47a5a1d2bf5
Andrew Lampinen <lampinen@stanford.edu>
2018-02-13 11:43:41 -0800

    shared input mode datasets

--- datasets.py
+++ datasets.py
@@ -51,3 +51,31 @@ if __name__ == "__main__":
     print()
     print(transformed_dataset(2, 3, 2))
 
+
+def random_vector_at_cos(initial_vec, cos_theta):
+    """Calculates a uniformly distributed unit vector at a given cosine of angle to the initial vector"""
+    initial_vec /= numpy.linalg.norm(initial_vec)
+    random_orthogonal_vec = numpy.random.randn(len(initial_vec))
+    random_orthogonal_vec /= numpy.linalg.norm(random_orthogonal_vec)
+    random_orthogonal_vec -= numpy.dot(initial_vec, random_orthogonal_vec) * initial_vec 
+    random_orthogonal_vec /= numpy.linalg.norm(random_orthogonal_vec)
+    new_vec = cos_theta * initial_vec + (1 - cos_theta) * random_orthogonal_vec
+    return new_vec
+
+
+def shared_input_modes_dataset(num_examples, num_outputs, num_domains, q):
+    """Returns a dataset of num_domains block domains with input modes related at strength q in [-1, 1] (where magnitude is what matters)"""
+    input_modes = random_orthogonal(num_examples)
+    task_0_strengths = numpy.array([numpy.sqrt(2./num_examples)] * (num_examples//2) + [0.]* (num_examples - num_examples//2))
+    task_strengths = [task_0_strengths] + [random_vector_at_cos(task_0_strengths, q) for j in xrange(num_domains - 1)]
+    
+    domain_sets = [numpy.matmul(numpy.transpose(random_dataset(num_examples, num_outputs)), numpy.matmul(numpy.diag(this_task_strengths), input_modes)) for this_task_strengths in task_strengths]
+
+    y_data = numpy.concatenate(domain_sets, axis=1)
+    x_data = numpy.eye(len(y_data))
+    return x_data, y_data
+
+def noisy_shared_input_modes_dataset(num_examples, num_outputs, num_domains, q, noise_var=0.1):
+    x_data, y_data = shared_input_modes_dataset(num_examples, num_outputs, num_domains, q)
+    y_data_noisy = y_data + noise_var/num_outputs * numpy.random.standard_normal(numpy.shape(y_data))
+    return x_data, y_data, y_data_noisy
