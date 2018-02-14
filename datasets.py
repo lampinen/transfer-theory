ab6c23376c393a721faa00e5995861886e30bb2f
Andrew Lampinen <lampinen@stanford.edu>
2018-02-13 16:59:52 -0800

    interpolation of shared inputs actually works in some sense of the word

--- datasets.py
+++ datasets.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division 
 import numpy
+from scipy.linalg import block_diag
 from orthogonal_matrices import random_orthogonal
 
 def random_dataset(num_examples, num_outputs):
@@ -62,20 +63,32 @@ def random_vector_at_cos(initial_vec, cos_theta):
     new_vec = cos_theta * initial_vec + (1 - cos_theta) * random_orthogonal_vec
     return new_vec
 
+def random_rotation_matrix(theta, n):
+    """Generates a rotation of an angle theta in n dimensions about a random vector"""
+    P = random_orthogonal(n)
+    R = block_diag(numpy.array([[numpy.cos(theta), -numpy.sin(theta)],[numpy.sin(theta), numpy.cos(theta)]]), numpy.eye(n-2)) 
+    return numpy.matmul(numpy.transpose(P),  numpy.matmul(R, P))
 
 def shared_input_modes_dataset(num_examples, num_outputs, num_domains, q):
-    """Returns a dataset of num_domains block domains with input modes related at strength q in [-1, 1] (where magnitude is what matters)"""
+    """Returns a dataset of num_domains block domains with input modes related at angle q"""
+    if num_outputs < num_examples:
+        raise ValueError("Less than full rank tasks are not supported at the moment (the appropriate truncation of the permuted S matrix is kinda annoying)")
     input_modes = random_orthogonal(num_examples)
-    task_0_strengths = numpy.array([numpy.sqrt(2./num_examples)] * (num_examples//2) + [0.]* (num_examples - num_examples//2))
-    task_strengths = [task_0_strengths] + [random_vector_at_cos(task_0_strengths, q) for j in xrange(num_domains - 1)]
+    strengths = numpy.array([numpy.sqrt(2./num_examples)] * (num_examples//2) + [0.]* (num_examples - num_examples//2))
     
-    domain_sets = [numpy.matmul(numpy.transpose(random_dataset(num_examples, num_outputs)), numpy.matmul(numpy.diag(this_task_strengths), input_modes)) for this_task_strengths in task_strengths]
+    def _strengths_to_S(strengths, num_outputs=num_outputs):
+        return numpy.concatenate((numpy.diag(strengths), numpy.zeros((num_outputs-len(strengths), num_examples))), axis=0)
+
+    S = _strengths_to_S(strengths)
+
+    rotations = [numpy.eye(num_examples)] + [random_rotation_matrix(q, num_examples) for i in xrange(num_domains - 1)]
+    domain_sets = [numpy.transpose(numpy.matmul(random_orthogonal(num_outputs), numpy.matmul(S, numpy.matmul(this_rotation, input_modes)))) for this_rotation in rotations]
 
     y_data = numpy.concatenate(domain_sets, axis=1)
     x_data = numpy.eye(len(y_data))
-    return x_data, y_data
+    return x_data, y_data, input_modes
 
 def noisy_shared_input_modes_dataset(num_examples, num_outputs, num_domains, q, noise_var=0.1):
-    x_data, y_data = shared_input_modes_dataset(num_examples, num_outputs, num_domains, q)
+    x_data, y_data, input_modes = shared_input_modes_dataset(num_examples, num_outputs, num_domains, q, task_0_strengths)
     y_data_noisy = y_data + noise_var/num_outputs * numpy.random.standard_normal(numpy.shape(y_data))
-    return x_data, y_data, y_data_noisy
+    return x_data, y_data, y_data_noisy, input_modes
