1393eb25d243f6f282c41ccb6687b9ab702428f2
Andrew Lampinen <lampinen@stanford.edu>
2018-05-12 15:54:23 -0700

    Rank 1 SNR comparisons

--- running_single_generalization_no_tf.py
+++ running_single_generalization_no_tf.py
@@ -8,19 +8,20 @@ from orthogonal_matrices import random_orthogonal
 ### Parameters
 num_examples = 100
 output_sizes = [num_examples] #[10, 50, 100, 200, 400]
-sigma_zs = [1, 5, 10, 100] 
-num_runs = 4
+sigma_zs = [1] 
+num_runs = 10
 learning_rate = 0.001
 num_epochs = 5000
 num_hidden = num_examples
 batch_size = num_examples
-filename_prefix = "single_generalization_full_results_notf_wideoutput/"
+filename_prefix = "single_generalization_comparison_results/"
 input_type = "one_hot" # one_hot, orthogonal, gaussian
-track_SVD = True
+track_SVD = False
 save_every = 5
 epsilon = 1e-5
 singular_value_multiplier = 10 
-N_2_bar = 4 # rank of teacher
+singular_value_multipliers = [float(x) for x in range(1, 11)]
+N_2_bar = 1 # rank of teacher
 
 ###
 #var_scale_init = tf.contrib.layers.variance_scaling_initializer(factor=2*np.sqrt(epsilon), mode='FAN_AVG')
@@ -28,80 +29,91 @@ N_2_bar = 4 # rank of teacher
 for run_i in xrange(num_runs):
     for sigma_z in sigma_zs:
         noise_var = sigma_z**2
-        for output_size in output_sizes:
-            
-            scaled_noise_var = noise_var/output_size
-            np.random.seed(run_i)
-            print("Now running noise_var: %.2f, output_size: %i, run: %i" % (noise_var, output_size, run_i))
-  
-            num_input = num_examples
-            num_output = output_size
-            if input_type == "one_hot":
-                x_data, y_data, noisy_y_data, input_modes = datasets.noisy_SVD_dataset(num_examples, output_size, noise_var=scaled_noise_var, singular_value_multiplier=singular_value_multiplier, num_nonempty=N_2_bar) 
-            else:
-                x_data, y_data, noisy_y_data, input_modes = datasets.noisy_SVD_dataset_different_inputs(num_examples, output_size, noise_var=scaled_noise_var, input_type="orthogonal", singular_value_multiplier=singular_value_multiplier, num_nonempty=1) 
+	for singular_value_multiplier in singular_value_multipliers:
+	    for aligned in [True, False]:
+		for output_size in output_sizes:
+		    
+		    scaled_noise_var = noise_var/output_size
+		    np.random.seed(run_i)
+		    print("Now running noise_var: %.2f, output_size: %i, svm: %f, alignment: %s, run: %i" % (noise_var, output_size, singular_value_multiplier, aligned, run_i))
+	  
+		    num_input = num_examples
+		    num_output = output_size
+		    if input_type == "one_hot":
+			x_data, y_data, noisy_y_data, input_modes = datasets.noisy_SVD_dataset(num_examples, output_size, noise_var=scaled_noise_var, singular_value_multiplier=singular_value_multiplier, num_nonempty=N_2_bar) 
+		    else:
+			x_data, y_data, noisy_y_data, input_modes = datasets.noisy_SVD_dataset_different_inputs(num_examples, output_size, noise_var=scaled_noise_var, input_type="orthogonal", singular_value_multiplier=singular_value_multiplier, num_nonempty=N_2_bar) 
 
-            noisy_y_data = noisy_y_data.transpose()
-            y_data = y_data.transpose()
+		    noisy_y_data = noisy_y_data.transpose()
+		    y_data = y_data.transpose()
 
-            y_data_frob_squared = np.sum(y_data**2)
-  
-            if track_SVD:
-                U_bar, S_bar, V_bar = np.linalg.svd(y_data, full_matrices=False)
-                U_bar = U_bar[:, :1]; S_bar = S_bar[:1]; V_bar = V_bar[:1, :]; # save some computation later -- other singular values are zero
-                U_hat, S_hat, V_hat = np.linalg.svd(noisy_y_data, full_matrices=False)
-                U_hat_base = U_hat; V_hat_base=V_hat;
-                U_hat = U_hat[:, :1]; S_hat = S_hat[:1]; V_hat = V_hat[:1, :]; 
+		    y_data_frob_squared = np.sum(y_data**2)
+	  
+		    if track_SVD:
+			U_bar, S_bar, V_bar = np.linalg.svd(y_data, full_matrices=False)
+			U_bar = U_bar[:, :N_2_bar]; S_bar = S_bar[:N_2_bar]; V_bar = V_bar[:N_2_bar, :]; # save some computation later -- other singular values are zero
+			U_hat, S_hat, V_hat = np.linalg.svd(noisy_y_data, full_matrices=False)
+			U_hat_base = U_hat; V_hat_base=V_hat;
+			U_hat = U_hat[:, :N_2_bar]; S_hat = S_hat[:N_2_bar]; V_hat = V_hat[:N_2_bar, :]; 
 
-            np.savetxt(filename_prefix + "noise_var_%.2f_output_size_%i_run_%i_y_data.csv"% (noise_var, output_size, run_i), y_data, delimiter=",")
-            np.savetxt(filename_prefix + "noise_var_%.2f_output_size_%i_run_%i_noisy_y_data.csv"% (noise_var, output_size, run_i), noisy_y_data, delimiter=",")
-            if input_type != "one_hot":
-                np.savetxt(filename_prefix + "noise_var_%.2f_output_size_%i_run_%i_x_data.csv"% (noise_var, output_size, run_i), x_data, delimiter=",")
- 
-            
-            # initialize weights as random orthogonal matrices and scale so
-            # product has singular values of epsilon
-            W21 = np.sqrt(epsilon) * random_orthogonal(num_input)[:num_hidden, :] 
-            W32 = np.sqrt(epsilon) * random_orthogonal(num_output)[:, :num_hidden] 
-        
-            sigma_31 = np.matmul(noisy_y_data, x_data.transpose())
-            sigma_11 = np.matmul(x_data, x_data.transpose())
+		    np.savetxt(filename_prefix + "noise_var_%.2f_output_size_%i_svm_%f_aligned_%s_run_%i_y_data.csv"% (noise_var, output_size, singular_value_multiplier, aligned, run_i), y_data, delimiter=",")
+		    np.savetxt(filename_prefix + "noise_var_%.2f_output_size_%i_svm_%f_aligned_%s_run_%i_noisy_y_data.csv"% (noise_var, output_size, singular_value_multiplier, aligned, run_i), noisy_y_data, delimiter=",")
+		    if input_type != "one_hot":
+		      np.savetxt(filename_prefix + "noise_var_%.2f_output_size_%i_svm_%f_aligned_%s_run_%i_x_data.csv"% (noise_var, output_size, singular_value_multiplier, aligned, run_i), x_data, delimiter=",")
+	 
+		     
+		    if aligned:
+			# initialize weights aligned with noisy data moddes and scale so
+			# product has singular values of epsilon
+			if not track_SVD:
+			    U_hat, S_hat, V_hat = np.linalg.svd(noisy_y_data, full_matrices=False)
+			    U_hat_base = U_hat; V_hat_base=V_hat;
+			W21 = np.sqrt(epsilon) * V_hat_base
+			W32 = np.sqrt(epsilon) * U_hat_base
+		    else:
+			# initialize weights as random orthogonal matrices and scale so
+			# product has singular values of epsilon
+			W21 = np.sqrt(epsilon) * random_orthogonal(num_input)[:num_hidden, :] 
+			W32 = np.sqrt(epsilon) * random_orthogonal(num_output)[:, :num_hidden] 
+		
+		    sigma_31 = np.matmul(noisy_y_data, x_data.transpose())
+		    sigma_11 = np.matmul(x_data, x_data.transpose())
 
-            
-            def train_epoch():
-                global W21, W32
-                l = sigma_31 - np.matmul(np.matmul(W32, W21), sigma_11)
-                W21 += learning_rate * np.matmul(W32.transpose(), l) 
-                W32 += learning_rate * np.matmul(l, W21.transpose()) 
-                
+		    
+		    def train_epoch():
+			global W21, W32
+			l = sigma_31 - np.matmul(np.matmul(W32, W21), sigma_11)
+			W21 += learning_rate * np.matmul(W32.transpose(), l) 
+			W32 += learning_rate * np.matmul(l, W21.transpose()) 
+			
 
-            def evaluate():
-                global W21, W32
-                curr_loss = np.sum(np.square(y_data - np.matmul(np.matmul(W32, W21), x_data)))
-                return curr_loss/y_data_frob_squared # appropriate normalization
-            
-                
-            if track_SVD:
-                fsvd = open(filename_prefix + "noise_var_%.2f_output_size_%i_run_%i_SVD_track.csv" % (noise_var, output_size, run_i), "w")
-                fsvd.write("epoch, " + ", ".join(["s%i"%i for i in range(1)]) + ", " + ", ".join(["U%iUhat%i" %(i,j) for i in range(1) for j in range(1)]) + "\n")
-            with open(filename_prefix + "noise_var_%.2f_output_size_%i_run_%i.csv" % (noise_var, output_size, run_i), "w") as fout:
+		    def evaluate():
+			global W21, W32
+			curr_loss = np.sum(np.square(y_data - np.matmul(np.matmul(W32, W21), x_data)))
+			return curr_loss/y_data_frob_squared # appropriate normalization
+		    
+			
+		    if track_SVD:
+			fsvd = open(filename_prefix + "noise_var_%.2f_output_size_%i_svm_%f_aligned_%s_run_%i_SVD_track.csv" % (noise_var, output_size, singular_value_multiplier, aligned, run_i), "w")
+			fsvd.write("epoch, " + ", ".join(["s%i"%i for i in range(1)]) + ", " + ", ".join(["U%iUhat%i" %(i,j) for i in range(1) for j in range(1)]) + "\n")
+		    with open(filename_prefix + "noise_var_%.2f_output_size_%i_svm_%f_aligned_%s_run_%i.csv" % (noise_var, output_size, singular_value_multiplier, aligned, run_i), "w") as fout:
 
-                fout.write("epoch, loss\n")
-                fout.write("%i, %f\n" % (0, evaluate()))
-                for epoch_i in xrange(1, num_epochs + 1):
-                    train_epoch()	
-                    curr_loss = evaluate()
-                    if epoch_i % save_every == 0:
-                        fout.write("%i, %f\n" % (epoch_i, curr_loss))
-                        print("%i, %f" % (epoch_i, curr_loss))
-                        if track_SVD:
-                            curr_output = np.matmul(np.matmul(W32, W21), x_data)
-                            U, S, V = np.linalg.svd(curr_output, full_matrices=False)
-                            U = U[:, :1]; S = S[:1]; V = V[:1, :]; 
-                            norm_S = S/S_hat
-                            U_dots = np.dot(U.transpose(), U_hat).flatten()
-                            fsvd.write("%i, " % epoch_i + ", ".join(["%f" for i in range(1)]) % tuple(norm_S) + ", " + ", ".join(["%f" for i in range(1) for j in range(1)]) % tuple(U_dots) + "\n")
+			fout.write("epoch, loss\n")
+			fout.write("%i, %f\n" % (0, evaluate()))
+			for epoch_i in xrange(1, num_epochs + 1):
+			    train_epoch()	
+			    curr_loss = evaluate()
+			    if epoch_i % save_every == 0:
+				fout.write("%i, %f\n" % (epoch_i, curr_loss))
+				print("%i, %f" % (epoch_i, curr_loss))
+				if track_SVD:
+				    curr_output = np.matmul(np.matmul(W32, W21), x_data)
+				    U, S, V = np.linalg.svd(curr_output, full_matrices=False)
+				    U = U[:, :1]; S = S[:1]; V = V[:1, :]; 
+				    norm_S = S/S_hat
+				    U_dots = np.dot(U.transpose(), U_hat).flatten()
+				    fsvd.write("%i, " % epoch_i + ", ".join(["%f" for i in range(1)]) % tuple(norm_S) + ", " + ", ".join(["%f" for i in range(1) for j in range(1)]) % tuple(U_dots) + "\n")
 
-            if track_SVD:
-                fsvd.close()
-  
+		    if track_SVD:
+			fsvd.close()
+    
--- theory_data_calculation.py
+++ theory_data_calculation.py
@@ -7,20 +7,21 @@ from theory_functions import *
 ### Parameters
 num_examples = 100
 output_sizes = [100] 
-sigma_zs = [1, 5, 10, 100]
+sigma_zs = [1]
 #num_runs = 10 
 learning_rate = 0.001
 num_epochs = 5000
 num_hidden = num_examples 
 #batch_size = num_examples
-filename_prefix = "single_generalization_full_results_notf_smallstud/"
+filename_prefix = "single_generalization_comparison_results/"
 #input_type = "one_hot" # one_hot, orthogonal, gaussian
 #track_SVD = True
 save_every = 5
 singular_value_multiplier = 10
+singular_value_multipliers = range(1,11)
 epsilon = 1e-5
 delta_x = 0.001 # for the numerical integration of M-P dist
-N_2_bar = 4 # number of teacher modes
+N_2_bar = 1 # number of teacher modes
 ### 
 
 base_singular_values = [float(i) for i in range(N_2_bar, 0, -1)] 
@@ -38,45 +39,46 @@ def get_noise_multiplier(s_bar, sigma_z):
 
 
 for sigma_z in sigma_zs:
-    noise_var = sigma_z**2
+    for singular_value_multiplier in singular_value_multipliers:
+	noise_var = sigma_z**2
 
-    singular_values = [s * singular_value_multiplier for s in base_singular_values]
+	singular_values = [s * singular_value_multiplier for s in base_singular_values]
 
-    y_frob_norm_sq = np.sum([s**2 for s in singular_values])
+	y_frob_norm_sq = np.sum([s**2 for s in singular_values])
 
-    N_2 = num_hidden
-    N_3 = N_1 = num_examples
-    tau = 1./learning_rate
-    sigma_z = np.sqrt(noise_var)
+	N_2 = num_hidden
+	N_3 = N_1 = num_examples
+	tau = 1./learning_rate
+	sigma_z = np.sqrt(noise_var)
 
-    s_hats = s_hat(singular_values, sigma_z)
-    s_bar = np.array(singular_values)
-    noise_multiplier = get_noise_multiplier(s_bar, sigma_z) 
-    with open(filename_prefix + "noise_var_%.2f_theory_track.csv" % (noise_var), "w") as fout:
-        fout.write("epoch, generalization_error, s0\n")
-        for epoch_i in xrange(1, num_epochs + 1, save_every):
+	s_hats = s_hat(singular_values, sigma_z)
+	s_bar = np.array(singular_values)
+	noise_multiplier = get_noise_multiplier(s_bar, sigma_z) 
+	with open(filename_prefix + "noise_var_%.2f_svm_%f_theory_track.csv" % (noise_var, singular_value_multiplier), "w") as fout:
+	    fout.write("epoch, generalization_error, s0\n")
+	    for epoch_i in xrange(1, num_epochs + 1, save_every):
 
-            sot = np.array(s_of_t(s_hats, epoch_i, epsilon, tau))
-            
-            generr = (N_2-len(singular_values))*numeric_integral_mp(delta_x*sigma_z, epoch_i, 0, 2*sigma_z) # number of points in integral estimate is constant in sigma_z 
-            generr += np.sum(sot**2) 
-            generr += y_frob_norm_sq
-            generr -= 2 * np.sum(sot * s_bar * noise_multiplier) 
-            generr /= y_frob_norm_sq
-            print("%i, %f, %f" % (epoch_i, generr, sot[0]))
-            fout.write("%i, %f, %f\n" % (epoch_i, generr, sot[0]))
+		sot = np.array(s_of_t(s_hats, epoch_i, epsilon, tau))
+		
+		generr = (N_2-len(singular_values))*numeric_integral_mp(delta_x*sigma_z, epoch_i, 0, 2*sigma_z) # number of points in integral estimate is constant in sigma_z 
+		generr += np.sum(sot**2) 
+		generr += y_frob_norm_sq
+		generr -= 2 * np.sum(sot * s_bar * noise_multiplier) 
+		generr /= y_frob_norm_sq
+		print("%i, %f, %f" % (epoch_i, generr, sot[0]))
+		fout.write("%i, %f, %f\n" % (epoch_i, generr, sot[0]))
 
-#    print()
-#    print()
-#    print(s_hats)
-#    print(noise_multiplier)
-#    generr = 0 #numeric_integral_mp(0.001, epoch_i, 0, 2)
-#    generr += np.sum(np.array(s_hats)**2) 
-#    print(generr)
-#    generr += y_frob_norm_sq
-#    print(generr)
-#    generr -= 2 * np.sum(s_hats * s_bar * noise_multiplier) 
-#    print(generr)
-#    generr /= y_frob_norm_sq
-#    print(generr) 
-#        
+    #    print()
+    #    print()
+    #    print(s_hats)
+    #    print(noise_multiplier)
+    #    generr = 0 #numeric_integral_mp(0.001, epoch_i, 0, 2)
+    #    generr += np.sum(np.array(s_hats)**2) 
+    #    print(generr)
+    #    generr += y_frob_norm_sq
+    #    print(generr)
+    #    generr -= 2 * np.sum(s_hats * s_bar * noise_multiplier) 
+    #    print(generr)
+    #    generr /= y_frob_norm_sq
+    #    print(generr) 
+    #        
